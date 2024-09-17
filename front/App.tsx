5fc6431aef6fc9e0d9e9b8e380b1741a4bf10944
devmk1090 <devmk1090@gmail.com>
2024-09-18 04:49:31 +0900

    - View만 사용하면 IOS의 노치 부분이 짤릴 수가 있기 때문에 SafeAreaView를 View위에 선언 - Flex box(https://ingg.dev/flexbox/) 사용. Android의 ConstraintLayout 같은 느낌

--- front/App.tsx
+++ front/App.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 function App() {
+  const [name, setName] = useState('')
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    setName(text);
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>텍스트</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={handleChangeInput}
+          />
+          <Text>이름</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'yellow',
+    flex: 1,
   },
   input: {
+    flex: 1,
     borderWidth: 2,
     borderColor: 'black',
+    height: 50,
+    width: 100,
+
   },
   inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
   }
 });
 
