dc8ac73854b4b167c1d74ac822eb7bc7876126d5
devmk1090 <devmk1090@gmail.com>
2024-10-20 01:49:36 +0900

    장소 등록 페이지 (5) - 커스텀 마커 구현 - declare 구문을 이용하여 Props를 커스텀

--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
-import {StackScreenProps} from '@react-navigation/stack';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 import Octicons from 'react-native-vector-icons/Octicons';
 
-import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
-import {colors, mapNavigations} from '@/constants';
+import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
+import { colors, mapNavigations } from '@/constants';
 import InputField from '@/components/InputField';
 import CustomButton from '@/components/CustomButton';
 import useForm from '@/hooks/useForm';
@@ -14,14 +14,15 @@ import { createPost } from '@/api';
 import { MarkerColor } from '@/types';
 import useGetAddress from '@/hooks/useGetAddress';
 import useMutateCreatePost from '@/hooks/queries/useMutateCreatePost';
+import MarkerSelector from '@/components/MarkerSelector';
 
 type AddPostScreenProps = StackScreenProps<
   MapStackParamList,
   typeof mapNavigations.ADD_POST
 >;
 
-function AddPostScreen({route, navigation}: AddPostScreenProps) {
-  const {location} = route.params;
+function AddPostScreen({ route, navigation }: AddPostScreenProps) {
+  const { location } = route.params;
   const descriptionRef = useRef<TextInput | null>(null);
   const createPost = useMutateCreatePost();
   const address = useGetAddress(location);
@@ -37,6 +38,10 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
   const [markerColor, setMarkerColor] = useState<MarkerColor>('RED');
   const [score, setScore] = useState(5);
 
+  const handleSelectMarker = (name: MarkerColor) => {
+    setMarkerColor(name);
+  };
+  
   const handleSubmit = () => {
     const body = {
       date: new Date(),
@@ -47,13 +52,13 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
       imageUris: [],
     };
     createPost.mutate(
-      {address, ...location, ...body},
+      { address, ...location, ...body },
       {
         onSuccess: () => navigation.goBack(),
       },
     );
   };
-  
+
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => AddPostHeaderRight(handleSubmit),
@@ -62,39 +67,44 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
 
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.contentContainer}>
-      <View style={styles.inputContainer}>
-        <InputField
-          value={address}
-          disabled={true}
-          icon={
-            <Octicons name="location" size={16} color={colors.GRAY_500} />
-          }
-        />
-        <CustomButton variant="outlined" size="large" label={'날짜 선택'} />
-        <InputField
-          {...addPost.getTextInputProps('title')}
-          error={addPost.errors.title}
-          touched={addPost.touched.title}
-          placeholder="제목을 입력하세요."
-          returnKeyType="next"
-          blurOnSubmit={false}
-          onSubmitEditing={() => {
-            descriptionRef.current?.focus();
-          }}
-        />
-        <InputField
-          {...addPost.getTextInputProps('description')}
-          error={addPost.errors.description}
-          touched={addPost.touched.description}
-          ref={descriptionRef}
-          placeholder="기록하고 싶은 내용을 입력하세요. (선택)"
-          returnKeyType="next"
-          multiline
-        />
-      </View>
-    </ScrollView>
-  </SafeAreaView>
+      <ScrollView style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <InputField
+            value={address}
+            disabled={true}
+            icon={
+              <Octicons name="location" size={16} color={colors.GRAY_500} />
+            }
+          />
+          <CustomButton variant="outlined" size="large" label={'날짜 선택'} />
+          <InputField
+            {...addPost.getTextInputProps('title')}
+            error={addPost.errors.title}
+            touched={addPost.touched.title}
+            placeholder="제목을 입력하세요."
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onSubmitEditing={() => {
+              descriptionRef.current?.focus();
+            }}
+          />
+          <InputField
+            {...addPost.getTextInputProps('description')}
+            error={addPost.errors.description}
+            touched={addPost.touched.description}
+            ref={descriptionRef}
+            placeholder="기록하고 싶은 내용을 입력하세요. (선택)"
+            returnKeyType="next"
+            multiline
+          />
+          <MarkerSelector
+            score={score}
+            markerColor={markerColor}
+            onPressMarker={handleSelectMarker}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
