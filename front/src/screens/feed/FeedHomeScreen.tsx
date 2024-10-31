f613c9f199c849a93bad8c95507d2c2e5c4531e9
devmk1090 <devmk1090@gmail.com>
2024-11-01 02:22:16 +0900

    무한 스크롤 구현 (2) - FlatList를 이용하여 무한 스크롤 구현

--- front/src/screens/feed/FeedHomeScreen.tsx
+++ front/src/screens/feed/FeedHomeScreen.tsx
@@ -4,7 +4,6 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 function FeedHomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <FeedList />
     </SafeAreaView>
   );
 }
