b43c96fd8bdb62e9eab73ba83c31314debb8f86b
devmk1090 <devmk1090@gmail.com>
2024-11-20 01:08:33 +0900

    Bottom Tab Navigation - 피드 바텀 탭 내비게이션 구현

--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -8,10 +8,11 @@ import { NavigatorScreenParams, RouteProp } from '@react-navigation/native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import CustomDrawerContent from './CustomDrawerContent';
 import FeedStackNavigator, { FeedStackParamList } from '../stack/FeedStackNavigator';
+import FeedTabNavigator, { FeedTabParamList } from '../tab/FeedTabNavigator';
 
 export type MainDrawerParamList = {
   [mainNavigations.HOME]: NavigatorScreenParams<MapStackParamList>;
-  [mainNavigations.FEED]: NavigatorScreenParams<FeedStackParamList>;
+  [mainNavigations.FEED]: NavigatorScreenParams<FeedTabParamList>;
   [mainNavigations.CALENDAR]: undefined;
 };
 
@@ -73,7 +74,7 @@ function MainDrawerNavigator() {
         }} />
       <Drawer.Screen
         name={mainNavigations.FEED}
-        component={FeedStackNavigator}
+        component={FeedTabNavigator}
         options={{
           title: '피드'
         }} />
---
+++ front/src/screens/feed/FeedFavoriteScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import {colors} from '@/constants';
+
+function FeedFavoriteScreen() {
+    return (
+      <SafeAreaView style={styles.container}>
+      </SafeAreaView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.WHITE,
+    },
+  });
+export default FeedFavoriteScreen;
