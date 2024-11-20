a983d9086ff249c95e773f250747152e30a03df4
devmk1090 <devmk1090@gmail.com>
2024-11-21 01:26:42 +0900

    Bottom Tab Navigation (2) - 추후 피드 검색 화면도 구현해볼 것

--- front/src/screens/feed/FeedFavoriteScreen.tsx
+++ front/src/screens/feed/FeedFavoriteScreen.tsx
@@ -6,6 +6,7 @@ import {colors} from '@/constants';
 function FeedFavoriteScreen() {
     return (
       <SafeAreaView style={styles.container}>
+        <FeedFavoriteScreen />
       </SafeAreaView>
     );
   }
