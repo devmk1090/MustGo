cb5d8ea3de4f30b5505346f102dd8105872ed2c9
devmk1090 <devmk1090@gmail.com>
2024-12-17 01:35:23 +0900

    캘린더 추가 - 월을 넘길때 깜박이는 현상 제거 - useQuery의 placeholderData 설정

--- front/src/hooks/queries/useGetCalendarPosts.ts
+++ front/src/hooks/queries/useGetCalendarPosts.ts
@@ -1,7 +1,7 @@
 import { ResponseCalendarPost, getCalendarPosts } from '@/api';
 import { queryKeys } from '@/constants';
 import { UseQueryCustomOptions } from '@/types';
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 function useGetCalendarPosts(
     year: number,
@@ -11,8 +11,11 @@ function useGetCalendarPosts(
     return useQuery({
         queryFn: () => getCalendarPosts(year, month),
         queryKey: [queryKeys.POST, queryKeys.GET_CALENDAR_POSTS, year, month],
+        placeholderData: keepPreviousData, // 월을 넘길때 깜박임 
         ...queryOptions,
     });
 }
+// for v4
+// keepPreviousData: true
 
 export default useGetCalendarPosts;
