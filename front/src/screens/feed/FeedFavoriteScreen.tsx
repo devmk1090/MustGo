933da62ffdb07945d90a4348fe92038635220d37
devmk1090 <devmk1090@gmail.com>
2025-05-05 03:41:33 +0900

    Suspense - 비동기 데이터를 선언적이고 깔끔하게 처리하기 위해 사용 - 로딩 상태를 컴포넌트 내에서 따로 처리할 필요 없이 fallback을 이용해 screen내에서 처리 가능 - Error Boundary와 같이 쓰면 더 좋음

--- front/src/screens/feed/FeedFavoriteScreen.tsx
+++ front/src/screens/feed/FeedFavoriteScreen.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
-import {colors} from '@/constants';
+import { colors } from '@/constants';
 import { ThemeMode } from '@/types';
 import useThemeStorage from '@/hooks/useThemeStorage';
+import Loader from '@/components/common/Loader';
+import { Suspense } from 'react';
 
 function FeedFavoriteScreen() {
   const { theme } = useThemeStorage()
   const styles = styling(theme)
 
-    return (
-      <SafeAreaView style={styles.container}>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Suspense fallback={<Loader />}>
         <FeedFavoriteScreen />
-      </SafeAreaView>
-    );
-  }
-  
-  const styling = (theme: ThemeMode) =>
+      </Suspense>
+    </SafeAreaView>
+  );
+}
+
+const styling = (theme: ThemeMode) =>
   StyleSheet.create({
     container: {
       flex: 1,
--- front/src/screens/feed/FeedHomeScreen.tsx
+++ front/src/screens/feed/FeedHomeScreen.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Loader from '@/components/common/Loader';
+import FeedList from '@/components/feed/FeedList';
+import React, { Suspense } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 function FeedHomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
+      <Suspense fallback={<Loader />}>
+        <FeedList />
+      </Suspense>
     </SafeAreaView>
   );
 }
