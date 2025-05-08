393bff591e84e61c10b38b23cd8fead8088b5f7b
devmk1090 <devmk1090@gmail.com>
2025-05-09 01:38:13 +0900

    ErrorBoundary - RootNavigator에 RetryErrorBoundary 설정 - Query에서 suspense를 사용하지 않는다면 throwOnError를 직접 설정

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import AuthStackNavigator from "../stack/AuthStackNavigator";
 import MainDrawerNavigator from "../drawer/MainDrawerNavigator";
 import useAuth from '../../hooks/queries/useAuth'
+import RetryErrorBoundary from '@/components/common/RetryErrorBoundary';
 
 function RootNavigator() {
     const isLogin = useAuth();
 
-    return <>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>
+    return <RetryErrorBoundary>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</RetryErrorBoundary>
     
 
 }
