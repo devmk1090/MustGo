73a4a77d5fcf2c5e4ac9867f32e3e9232ed7fbe2
devmk1090 <devmk1090@gmail.com>
2025-05-15 23:55:54 +0900

    Splash Screen - useGetRefreshToken에서 isPending을 return으로 받아 isLoginLoading 변수에 담음 - RootNavigator에서 isLoginLoading이 false라면 SplashScreen을 숨김

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AuthStackNavigator from "../stack/AuthStackNavigator";
 import MainDrawerNavigator from "../drawer/MainDrawerNavigator";
 import useAuth from '../../hooks/queries/useAuth'
 import RetryErrorBoundary from '@/components/common/RetryErrorBoundary';
+import SplashScreen from 'react-native-splash-screen';
 
 function RootNavigator() {
-    const isLogin = useAuth();
+    const { isLogin, isLoginLoading } = useAuth();
+
+    useEffect(() => {
+        if (!isLoginLoading) {
+            SplashScreen.hide()
+        }
+    }, [isLoginLoading]);
 
     return <RetryErrorBoundary>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</RetryErrorBoundary>
-    
+
 
 }
 
