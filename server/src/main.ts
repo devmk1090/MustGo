a822192901523942239d94a1714650bcc55f7a4a
devmk1090 <devmk1090@gmail.com>
2025-06-08 04:25:32 +0900

    initial commit - nestjs 프로젝트 생성 - typeorm 설치

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,21 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3030;
-
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.enableCors({
-  //     origin: ['https://...'],
-  //     credentials: true,
-  //   });
-  // } else {
-  //   app.enableCors({
-  //     origin: true,
-  //     credentials: true,
-  //   });
-  // }
-
-  await app.listen(port);
-  console.log(`http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
