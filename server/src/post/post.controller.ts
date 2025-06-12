351f83f2c46f33980bfc0a998f16d4d63834464b
devmk1090 <devmk1090@gmail.com>
2025-06-13 01:27:51 +0900

    post module - entity 작성 - getPosts 의 더미 응답값 postman에서 확인 - pgAdmin에서 entity table 확인

--- server/src/post/post.controller.ts
+++ server/src/post/post.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PostService } from './post.service';
 
 //요청을 처리하고 응답을 리턴
 
-@Controller('post')
-export class PostController {}
+@Controller()
+export class PostController {
+    constructor(private postService: PostService) {}
+
+    @Get('/posts')
+    getPosts() {
+        return this.postService.getPosts();
+    }
+}
--- server/src/post/post.service.ts
+++ server/src/post/post.service.ts
@@ -3,4 +3,8 @@ import { Injectable } from '@nestjs/common';
 //각 컨트롤러의 메서드에 해당하는 db작업 로직이 들어감
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    getPosts() {
+        return ['a게시글', 'b게시글',];
+    }
+}
