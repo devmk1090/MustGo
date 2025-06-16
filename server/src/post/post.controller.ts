ec6e9df6d41f481d0ef6077c067be578e929ce76
devmk1090 <devmk1090@gmail.com>
2025-06-17 02:00:06 +0900

    post module (1) - CreatePostDto 작성 - class-validator 설치

--- server/src/post/post.controller.ts
+++ server/src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 //요청을 처리하고 응답을 리턴
 
@@ -9,6 +10,12 @@ export class PostController {
 
     @Get('/posts')
     getPosts() {
-        return this.postService.getPosts();
+        return this.postService.getPosts
+        ();
+    }
+
+    @Post('/posts')
+    createPost(@Body() createPostDto: CreatePostDto) {
+        return this.postService.createPost();
     }
 }
--- server/src/post/post.service.ts
+++ server/src/post/post.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
 
 //각 컨트롤러의 메서드에 해당하는 db작업 로직이 들어감
 
 @Injectable()
 export class PostService {
-    getPosts() {
+    async getPosts() {
         return ['a게시글', 'b게시글',];
     }
+
+    async createPost(createPostDto: CreatePostDto) {
+        const {
+            latitude,
+            longitude,
+            color,
+            address,
+            title,
+            description,
+            date,
+            score,
+            imageUris,
+        } = createPostDto;
+
+    }
+
 }
