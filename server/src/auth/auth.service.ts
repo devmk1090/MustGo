e543ae092df36eedc9958da7dad0b1aa2d89dde2
devmk1090 <devmk1090@gmail.com>
2025-07-07 02:19:59 +0900

    Auth Module (2) - 사용자 비밀번호를 암호화 하기 위한 bcryptjs 설치 - 회원가입 구현

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,17 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User)
         private userRepository: Repository<User>,
-    ) {}
+    ) { }
 
     async signup(authDto: AuthDto) {
-        //
+        const { email, password } = authDto;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = this.userRepository.create({
+            email,
+            password: hashedPassword,
+            loginType: 'email',
+        });
+        try {
+            await this.userRepository.save(user);
+        } catch (error) {
+            console.log(error);
+            if (error.code === '23505') {
+                throw new ConflictException('User already exists');
+            }
+
+            throw new InternalServerErrorException('회원가입 도중 에러가 발생했습니다.');
+        }
+    }
+
+    async signin(authDto: AuthDto) {
+        const { email, password } = authDto;
+        const user = await this.userRepository.findOneBy({ email });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            throw new UnauthorizedException('이메일 또는 비밀번호가 일치하지 않습니다.');
+        }
     }
 }
